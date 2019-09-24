330cc3a324c14d248c9dbf407458f1b57b8592ee
Steven Bull <stevenwbulljr@gmail.com>
2019-09-24 10:45:44 -0500

    add port listener for local server

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const googleApps = require('./playstore');
+
 const app = express();
 
 app.use(morgan('common'));
 
+app.get('/apps', (req, res) => {
+  res.json('I\'m running');
+})
