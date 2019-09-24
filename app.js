5ffc0abb2291d846fa45c0d27f6ad735a230c367
Steven Bull <stevenwbulljr@gmail.com>
2019-09-24 11:04:01 -0500

    add validation for sort and search queries

--- app.js
+++ app.js
@@ -8,8 +8,19 @@ const app = express();
 app.use(morgan('common'));
 
 app.get('/apps', (req, res) => {
+  const { search, sort } = req.query;
 
-  
+  const sortParams = ['rating', 'app'];
+  const searchParams = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
+
+  if (sort && !sortParams.includes(sort)) {
+    return res.status(400).json('Error: sort must be by rating or app');
+  }
+
+  if (search && !searchParams.includes(search.toLowerCase())) {
+    return res.status(400).json('Error: please search by a valid genre. Such as: action, puzzle, strategy, casual, arcade, or card');
+  }
+
   res.json(googleApps);
 })
 
